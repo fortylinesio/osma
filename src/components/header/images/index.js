ae3216d9954f61b77a9470b59dbf9970da3bc4fe
Vladislav Pak <8289142-vpd.vlad@users.noreply.gitlab.com>
2021-03-18 11:27:42 +0600

    images

---
+++ src/components/header/images/index.js
@@ -0,0 +1,9 @@
+import img1 from './1.JPG'
+import img2 from "./2.JPG";
+import img3 from "./3.JPG";
+
+const images = {
+    img1, img2, img3
+}
+
+export default images
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,63 @@
 import React from "react";
+import img from "./images"
+import "./index.css";
 
 export const Header = () => {
-    return(
-        <header className="container">
-            <i class="fab fa-whatsapp"></i>
-        </header>
-    );
-}
+  return (
+    <header classNameName="container">
+      <div id="carouselExampleIndicators" className="carousel slide container" data-bs-ride="carousel">
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={img.img1} className="d-block" alt="img1" />
+          </div>
+          <div className="carousel-item">
+            <img src={img.img2} className="d-block" alt="img2" />
+          </div>
+          <div className="carousel-item">
+            <img src={img.img3} className="d-block" alt="img3" />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </header>
+  );
+};
